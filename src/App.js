1d04af6b88bfa2629a8c4978d60b7d5f2ff701ca
shaktigurung <mr.shakti.gurung@gmail.com>
2019-04-03 15:14:55 +1100

    Exhangerates fetched from server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import ExchangeRates from './components/ExchangeRates';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> GraphQL Practise </h1>
+        <ExchangeRates />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ApolloClient from "apollo-boost";
 import gql from "graphql-tag";
+import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io"
+    uri: "https://48p1r2roz4.sse.codesandbox.io"
 });
 
 client
@@ -22,7 +23,10 @@ client
   })
   .then(result => console.log(result));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
